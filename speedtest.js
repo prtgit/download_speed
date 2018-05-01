a5e9950d747c17effd8612a289d6b0535ac36c48
prtgit <prtanksa@ncsu.edu>
2018-05-01 12:01:37 -0400

    mtest var location changed

--- speedtest.js
+++ speedtest.js
@@ -2,7 +2,7 @@ var speedTest = require('speedtest-net');
 var http = require("http");
 var qs = require("querystring");
 var port = 3000;
-var test = speedTest({maxTime: 5000});
+
 
 var server = http.createServer(function (req, resp){
 	if(req.method == 'GET' && req.url == "/" && req.url != "/favicon.ico"){
@@ -10,6 +10,7 @@ var server = http.createServer(function (req, resp){
 		resp.statusCode = 200;
 		console.log("Request started");
 		resp.setHeader('Content-type','application/json');
+		var test = speedTest({maxTime: 5000});
 		test.on('data', data =>{
 			console.log("I am here");
 			console.dir(data);
